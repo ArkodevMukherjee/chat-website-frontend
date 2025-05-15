17367fce4e1bdabb22c706265e97259598af9b30
Arkodev Mukherjee <arkodev.programmer@gmail.com>
2025-05-15 18:05:24 +0530

    Url change has been made data

--- src/main/ChatRoom.jsx
+++ src/main/ChatRoom.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+const socket = io(process.env.REACT_APP_BACKEND_URL);
 
 const ChatRoom = () => {
   const { roomId } = useParams(); // Extract roomId from URL
